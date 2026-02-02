d45c9306b2f57b6ca6c33a104a20b8697c56b584
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2026-02-02 15:27:22 -0300

    Create import.sql file to add some data when start the project

--- src/main/java/com/algaworks/algafood_api/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood_api/domain/model/Cozinha.java
@@ -14,7 +14,6 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nom_cozinha")
     private String nome;
 
     public Long getId() {
