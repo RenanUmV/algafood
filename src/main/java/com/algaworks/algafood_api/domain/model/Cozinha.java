b5277ebab4d36d9196a50e7f91c6ad751b43d6d7
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2026-02-02 15:13:07 -0300

    Create Project, also create Cozinha and Restaurante entities

---
+++ src/main/java/com/algaworks/algafood_api/domain/model/Cozinha.java
@@ -0,0 +1,48 @@
+package com.algaworks.algafood_api.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Cozinha {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nom_cozinha")
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cozinha cozinha = (Cozinha) o;
+        return Objects.equals(id, cozinha.id) && Objects.equals(nome, cozinha.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
+}
---
+++ src/test/java/com/algaworks/algafood_api/AlgafoodApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.algaworks.algafood_api;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
