9770a186a7e96b681961fefa924c492eae322196
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2026-02-03 12:00:52 -0300

    Refactor Restaurante to use Lombok and implement RestauranteRepository

--- src/main/java/com/algaworks/algafood_api/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood_api/domain/model/Restaurante.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood_api.domain.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,52 +11,19 @@ import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Restaurante {
 
     @Id
+    @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
 
-    @Column(name = "tava_frete")
+    @Column(name = "taxa_frete")
     private BigDecimal taxaFrete;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public BigDecimal getTaxaFrete() {
-        return taxaFrete;
-    }
-
-    public void setTaxaFrete(BigDecimal taxaFrete) {
-        this.taxaFrete = taxaFrete;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Restaurante that = (Restaurante) o;
-        return Objects.equals(id, that.id) && Objects.equals(nome, that.nome) && Objects.equals(taxaFrete, that.taxaFrete);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, nome, taxaFrete);
-    }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+
+insert into restaurante (nome, taxa_frete) values ('La Ursa', 5.00);
+insert into restaurante (nome, taxa_frete) values ('Mineiro', 5.00);
