dc054f4174d845a4e80a997e3bbc76ff6b118cba
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2026-02-03 17:10:35 -0300

    Create Cidade

---
+++ src/main/java/com/algaworks/algafood_api/domain/model/Cidade.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood_api.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+public class Cidade {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Estado estado;
+}
---
+++ src/main/java/com/algaworks/algafood_api/jpa/ConsultaCidadeMain.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood_api.jpa;
+
+import com.algaworks.algafood_api.AlgafoodApiApplication;
+import com.algaworks.algafood_api.domain.model.Cidade;
+import com.algaworks.algafood_api.domain.model.Estado;
+import com.algaworks.algafood_api.domain.repository.CidadeRepository;
+import com.algaworks.algafood_api.domain.repository.EstadoRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class ConsultaCidadeMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CidadeRepository cidadeRepository = applicationContext.getBean(CidadeRepository.class);
+
+        List<Cidade> cidades = cidadeRepository.listar();
+
+        for (Cidade cidade : cidades) {
+            System.out.printf("%s - %s\n", cidade.getNome(), cidade.getEstado().getNome());
+        }
+    }
+}
