92e834b08ef27ad29a19b88f4b87852509bc3b28
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2026-02-05 10:04:49 -0300

    Create CozinhaController

---
+++ src/main/java/com/algaworks/algafood_api/api/controller/CozinhaController.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood_api.api.controller;
+
+import com.algaworks.algafood_api.domain.model.Cozinha;
+import com.algaworks.algafood_api.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar(){
+        return cozinhaRepository.listar();
+    }
+}
