9e4e44819953764c0999080d4813ab9c0fb83619
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2026-02-05 15:52:25 -0300

    Customize Xml representation

--- src/main/java/com/algaworks/algafood_api/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood_api/api/controller/CozinhaController.java
@@ -1,16 +1,21 @@
 package com.algaworks.algafood_api.api.controller;
 
+import com.algaworks.algafood_api.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood_api.domain.model.Cozinha;
 import com.algaworks.algafood_api.domain.repository.CozinhaRepository;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@JsonRootName("cozinha")
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas")//, produces = MediaType.APPLICATION_JSON_VALUE)
 public class CozinhaController {
 
     @Autowired
@@ -19,5 +24,15 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar(){
         return cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public CozinhasXmlWrapper listarXml(){
+        return new CozinhasXmlWrapper(cozinhaRepository.listar());
+    }
+
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable("cozinhaId") Long id){
+        return cozinhaRepository.buscar(id);
     }
 }
--- src/main/java/com/algaworks/algafood_api/api/controller/EstadosController.java
+++ src/main/java/com/algaworks/algafood_api/api/controller/EstadosController.java
@@ -2,15 +2,18 @@ package com.algaworks.algafood_api.api.controller;
 
 import com.algaworks.algafood_api.domain.model.Estado;
 import com.algaworks.algafood_api.domain.repository.EstadoRepository;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@JsonRootName("estado")
 @RestController
-@RequestMapping(value = "/estados")
+@RequestMapping(value = "/estados")//, produces = MediaType.APPLICATION_JSON_VALUE)
 public class EstadosController {
 
     @Autowired
@@ -20,4 +23,5 @@ public class EstadosController {
     public List<Estado> listar(){
         return estadoRepository.listar();
     }
+
 }
