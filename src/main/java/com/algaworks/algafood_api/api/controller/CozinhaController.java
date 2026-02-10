ae91209ca4f15e5c2e4fc15996e509435609aec1
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2026-02-10 11:01:52 -0300

    Create Post method to Cozinha

--- src/main/java/com/algaworks/algafood_api/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood_api/api/controller/CozinhaController.java
@@ -5,11 +5,15 @@ import com.algaworks.algafood_api.domain.model.Cozinha;
 import com.algaworks.algafood_api.domain.repository.CozinhaRepository;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -41,5 +45,11 @@ public class CozinhaController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha){
+        return cozinhaRepository.salvar(cozinha);
     }
 }
