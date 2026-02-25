153fee2f6bdd319a5336567d50e687a3ad627681
RenanUmV <renan.uchoa.m@gmail.com>
2026-02-24 23:37:49 -0300

    Create Put method to Restaurante

--- src/main/java/com/algaworks/algafood_api/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood_api/api/controller/RestauranteController.java
@@ -4,6 +4,7 @@ import com.algaworks.algafood_api.domain.exception.EntidadeNaoEncontradaExceptio
 import com.algaworks.algafood_api.domain.model.Restaurante;
 import com.algaworks.algafood_api.domain.repository.RestauranteRepository;
 import com.algaworks.algafood_api.domain.service.CadastroRestauranteService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,6 +46,26 @@ public class RestauranteController {
             return ResponseEntity.status(HttpStatus.CREATED)
                     .body(restaurante);
         }catch (EntidadeNaoEncontradaException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping({"restauranteId"})
+    public ResponseEntity<?> atualizar(@PathVariable("restauranteId") Long restauranteId,
+                                                 @RequestBody Restaurante restaurante){
+        try{
+            Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+
+            if(restauranteAtual != null){
+                BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+
+                restauranteAtual = cadastroRestauranteService.salvar(restaurante);
+
+                return ResponseEntity.ok(restauranteAtual);
+            }
+            return ResponseEntity.notFound().build();
+
+        }catch(EntidadeNaoEncontradaException e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
