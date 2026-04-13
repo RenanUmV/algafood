2bd6de4ee2322c8148f1bcd4db09da4713ed0218
Renan Vasconcelos <renan.uchoa.m@gmail.com>
2026-04-13 18:09:52 -0300

    Implement a simple query using criteria API

--- src/main/java/com/algaworks/algafood_api/infrasctructure/jpa/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood_api/infrasctructure/jpa/RestauranteRepositoryImpl.java
@@ -8,6 +8,8 @@ import org.springframework.util.StringUtils;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
@@ -21,30 +23,13 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
     @Override
     public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
 
-        var jpql = new StringBuilder();
-        jpql.append("from Restaurante where 0 = 0 ");
+       CriteriaBuilder builder = entityManager.getCriteriaBuilder();
 
-        var parametros = new HashMap<String, Object>();
+       CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+       criteria.from(Restaurante.class);
 
-        if (StringUtils.hasLength(nome)){
-            jpql.append("and nome like :nome");
-            parametros.put(nome, "%" + nome + "%");
-        }
+       TypedQuery<Restaurante> query = entityManager.createQuery(criteria);
 
-        if (taxaFreteInicial != null){
-            jpql.append("and taxaFrete >= :taxaInicial");
-            parametros.put("taxaInicial", taxaFreteInicial);
-        }
-
-        if (taxaFreteFinal != null){
-            jpql.append("and taxaFrete <= :taxafinal");
-            parametros.put("taxafinal", taxaFreteFinal);
-        }
-
-        TypedQuery<Restaurante> query = entityManager.createQuery(jpql.toString(), Restaurante.class);
-
-        parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
-
-        return query.getResultList();
+       return query.getResultList();
     }
 }
